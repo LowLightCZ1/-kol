ca27f1744b8a4fea68d81037536e1a5289f982a8
LowLightCZ1 <155197914+LowLightCZ1@users.noreply.github.com>
2023-12-29 23:20:26 +0100

    Program 1: Porovnání čísel

---
+++ GidHub_Work.py
@@ -0,0 +1,14 @@
+x = int(input("Zadej hodnotu x "))
+y = int(input("Zadej hodnotu y "))
+
+if x > y:
+    print("x je větší")
+elif x == y:
+    print("rovno")
+else:
+    print("y je větší")
+    
+
+
+
+    
