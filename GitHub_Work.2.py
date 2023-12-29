4c105ebe2ab3a4b78086917c0f456b71ed5f2477
LowLightCZ1 <155197914+LowLightCZ1@users.noreply.github.com>
2023-12-29 23:45:41 +0100

    Program2_hádání čísel

--- GitHub_Work.2.py
+++ GitHub_Work.2.py
@@ -1,1 +1,14 @@
+import random
 
+x = random.randint(1, 50) 
+y = int(input("Zadej hodnotu y "))
+
+
+while y != x:
+    if x > y:
+        print("Myslím si větší")  
+    
+    else:
+        print("Myslím si menší")
+    y = int(input("Zadej hodnotu y "))
+print("Uhádl jsi :) ")
