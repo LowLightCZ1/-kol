44b059850492c330d1554bf6cc1ba755e26d9e31
LowLightCZ1 <155197914+LowLightCZ1@users.noreply.github.com>
2023-12-30 18:57:05 +0100

    Update GitHub_Work3.py

--- GitHub_Work3.py
+++ GitHub_Work3.py
@@ -1,1 +1,14 @@
+import random
 
+x = random.randint(1, 100) 
+y = int(input("Zadej hodnotu y "))
+
+  
+def sudy_lichy(num_1):
+    if (num_1 % 2) == 0:
+        return "{0} je sudé".format(num_1)
+    else:
+        return "{0} je liché".format(num_1)
+
+print(sudy_lichy(x))
+print(sudy_lichy(y))
