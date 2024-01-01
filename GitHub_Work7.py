be10fb55c7c804e97a2ecef2f04b2c7a0b2a35d4
LowLightCZ1 <155197914+LowLightCZ1@users.noreply.github.com>
2024-01-01 13:36:00 +0100

    Update GitHub_Work7.py

--- GitHub_Work7.py
+++ GitHub_Work7.py
@@ -1,1 +1,11 @@
+x = int(input("zadej váhu"))
+y = int(input("zadej výšku"))
+    
+bmi = x / ((y/100)* (y/100))
 
+if bmi < 19:
+    print("Máš podváhu")
+elif bmi <= 25:
+    print("Mormální váha")
+else: 
+    print("Máš nadváhu")
