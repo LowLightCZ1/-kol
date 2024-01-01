69f3a179756de77f501686a76074142d021d8288
LowLightCZ1 <155197914+LowLightCZ1@users.noreply.github.com>
2024-01-01 13:15:23 +0100

    Update GitHub_Work6.py

--- GitHub_Work6.py
+++ GitHub_Work6.py
@@ -1,1 +1,13 @@
+numbers = [1, -5, -9, 7, 12]
+
+# print(numbers.count(1))
+# print(numbers.count(5))
+# print(numbers.count(4))
+# print(numbers.count(6))
+
+for i in range(len(numbers)):
+    if i > 0:
+        
+    
+
 
