b70459ea0480e57821345ab9403fdf62a981fc80
LowLightCZ1 <155197914+LowLightCZ1@users.noreply.github.com>
2024-01-01 13:36:04 +0100

    Update GitHub_Work9.py

--- GitHub_Work9.py
+++ GitHub_Work9.py
@@ -1,1 +1,7 @@
+def list_swap(list, old_num, new_num):
+    for i in range (len(list)):
+        if list[i] == old_num:
+           list[i] = new_num
+    return list
 
+print(list_swap([1, 10, 5, 6, 7, 5, 45], 5, 0))
