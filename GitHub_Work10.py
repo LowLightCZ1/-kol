f4e5cc5556197a4447cefd015c9704c33e7b375c
LowLightCZ1 <155197914+LowLightCZ1@users.noreply.github.com>
2024-01-02 11:15:50 +0100

    Update GitHub_Work10.py

--- GitHub_Work10.py
+++ GitHub_Work10.py
@@ -1,11 +1,4 @@
-def card_hide(card_numb):
-    return 12 * "*" + card_numb[12:]
-hiden_card = []
-for i in range(len(card_num)):
-    if i < 12:
-        hiden_card.append("*")
-    else:
-        hiden_card.append(card_num[i])
-return "".join
-
+def card_hide(card_num):
+    return card_num[:4] + (len(card_num) - 4) * "*"
+   
 print(card_hide("132568834900123468"))
